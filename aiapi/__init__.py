b2f561af98e03dfb0a0eb2bea201327bf0071c7a
Geoffrey Smalling <cedralpass+git@gmail.com>
2024-03-15 14:42:28 -0700

    adding aiapi endpoints

---
+++ aiapi/__init__.py
@@ -0,0 +1,39 @@
+import os
+
+from flask import Flask, jsonify
+from environs import Env
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    #TODO: figure out better config
+    env = Env()
+    env.read_env()
+    app.config['API_KEY'] = env("API_KEY")
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        json_obj ={"key":"Hello, World! in JSON"}
+        return jsonify(json_obj)
+    return app
