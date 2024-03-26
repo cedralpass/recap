5a5ffd83e8924ccd785d4d86e7c44d87f83684ae
Geoffrey Smalling <cedralpass+git@gmail.com>
2024-03-26 12:33:48 -0700

    Moving Url for Ai Classificaiton to Env Variable

--- recap/aiapi_helper.py
+++ recap/aiapi_helper.py
@@ -1,9 +1,12 @@
 import httpx
+from environs import Env
 
 class AiApiHelper:
     @staticmethod
     def ClassifyUrl(url, reference):
-         request_data = {'url': url, 'ref_key': reference, 'secret':"abc123"}
-
-         r = httpx.post('http://recapa-farga-l9gassm1meed-42ef6e85215dea14.elb.us-west-2.amazonaws.com/classify_url', data=request_data, timeout=60)
-         return r.json()
+        env = Env()
+        env.read_env()
+        ai_url = env("RECAP_AI_API_URL")
+        request_data = {'url': url, 'ref_key': reference, 'secret':"abc123"}
+        r = httpx.post(ai_url, data=request_data, timeout=60)
+        return r.json()
